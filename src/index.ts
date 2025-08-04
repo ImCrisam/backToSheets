5c37379085fe88d9391f555f4e6b2018ba6488f8
imcrisam <Imcrisam@gmail.com>
2025-08-03 21:19:04 -0500

    fix: update start script to set NODE_ENV for production and refactor Google credentials loading

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 // src/index.ts
 import bodyParser from 'body-parser';
-import { google, sheets_v4 } from 'googleapis';
+import { google } from 'googleapis';
 import { readFileSync } from 'fs';
 import expressPkg, { Request, Response } from 'express';
 
-const credentials = JSON.parse(readFileSync('./service-account.json', 'utf-8'));
+let credentials: any;
+if (process.env.NODE_ENV === 'production') {
+  credentials = {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+  };
+} else {
+  credentials = JSON.parse(readFileSync('./service-account.json', 'utf-8'));
+}
 interface Item {
   id: string | number;
   description: string;
